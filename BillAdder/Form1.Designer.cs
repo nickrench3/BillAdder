af079db6d0d3e9c3051ecf0cac24b0d4bd52c5e1
Nick Rentschler <nickrench3@icloud.com>
2020-07-04 17:15:40 -0500

    Add project files.

---
+++ BillAdder/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace BillAdder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.HundredsText = new System.Windows.Forms.TextBox();
+            this.FiftysText = new System.Windows.Forms.TextBox();
+            this.TwentiesText = new System.Windows.Forms.TextBox();
+            this.TensText = new System.Windows.Forms.TextBox();
+            this.FivesText = new System.Windows.Forms.TextBox();
+            this.OnesText = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.TotalText = new System.Windows.Forms.TextBox();
+            this.EnterButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(52, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "How many?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(62, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "100\'s";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(62, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "50\'s";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(62, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "20\'s";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(62, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "10\'s";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label6.Location = new System.Drawing.Point(62, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "5\'s";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label7.Location = new System.Drawing.Point(62, 193);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "1\'s";
+            // 
+            // HundredsText
+            // 
+            this.HundredsText.Location = new System.Drawing.Point(133, 40);
+            this.HundredsText.Name = "HundredsText";
+            this.HundredsText.Size = new System.Drawing.Size(100, 20);
+            this.HundredsText.TabIndex = 0;
+            // 
+            // FiftysText
+            // 
+            this.FiftysText.Location = new System.Drawing.Point(133, 70);
+            this.FiftysText.Name = "FiftysText";
+            this.FiftysText.Size = new System.Drawing.Size(100, 20);
+            this.FiftysText.TabIndex = 1;
+            // 
+            // TwentiesText
+            // 
+            this.TwentiesText.Location = new System.Drawing.Point(133, 100);
+            this.TwentiesText.Name = "TwentiesText";
+            this.TwentiesText.Size = new System.Drawing.Size(100, 20);
+            this.TwentiesText.TabIndex = 2;
+            // 
+            // TensText
+            // 
+            this.TensText.Location = new System.Drawing.Point(133, 130);
+            this.TensText.Name = "TensText";
+            this.TensText.Size = new System.Drawing.Size(100, 20);
+            this.TensText.TabIndex = 3;
+            // 
+            // FivesText
+            // 
+            this.FivesText.Location = new System.Drawing.Point(133, 160);
+            this.FivesText.Name = "FivesText";
+            this.FivesText.Size = new System.Drawing.Size(100, 20);
+            this.FivesText.TabIndex = 4;
+            // 
+            // OnesText
+            // 
+            this.OnesText.Location = new System.Drawing.Point(133, 190);
+            this.OnesText.Name = "OnesText";
+            this.OnesText.Size = new System.Drawing.Size(100, 20);
+            this.OnesText.TabIndex = 5;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label8.Location = new System.Drawing.Point(52, 231);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 17);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Total";
+            // 
+            // TotalText
+            // 
+            this.TotalText.Location = new System.Drawing.Point(133, 228);
+            this.TotalText.Name = "TotalText";
+            this.TotalText.ReadOnly = true;
+            this.TotalText.Size = new System.Drawing.Size(100, 20);
+            this.TotalText.TabIndex = 8;
+            // 
+            // EnterButton
+            // 
+            this.EnterButton.Location = new System.Drawing.Point(158, 264);
+            this.EnterButton.Name = "EnterButton";
+            this.EnterButton.Size = new System.Drawing.Size(75, 23);
+            this.EnterButton.TabIndex = 9;
+            this.EnterButton.Text = "Enter";
+            this.EnterButton.UseVisualStyleBackColor = true;
+            this.EnterButton.Click += new System.EventHandler(this.EnterButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(65, 264);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 23);
+            this.ClearButton.TabIndex = 10;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.EnterButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 327);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.EnterButton);
+            this.Controls.Add(this.TotalText);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.OnesText);
+            this.Controls.Add(this.FivesText);
+            this.Controls.Add(this.TensText);
+            this.Controls.Add(this.TwentiesText);
+            this.Controls.Add(this.FiftysText);
+            this.Controls.Add(this.HundredsText);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Bill Adder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox HundredsText;
+        private System.Windows.Forms.TextBox FiftysText;
+        private System.Windows.Forms.TextBox TwentiesText;
+        private System.Windows.Forms.TextBox TensText;
+        private System.Windows.Forms.TextBox FivesText;
+        private System.Windows.Forms.TextBox OnesText;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox TotalText;
+        private System.Windows.Forms.Button EnterButton;
+        private System.Windows.Forms.Button ClearButton;
+    }
+}
+
---
+++ BillAdder/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BillAdder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetTextBoxes();
+        }
+
+        private void SetTextBoxes()
+        {
+            HundredsText.Text = "0";
+            FiftysText.Text = "0";
+            TwentiesText.Text = "0";
+            TensText.Text = "0";
+            FivesText.Text = "0";
+            OnesText.Text = "0";
+            TotalText.Text = "0";
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            int Hundreds = Convert.ToInt32(HundredsText.Text) * 100;
+            int Fiftys = Convert.ToInt32(FiftysText.Text) * 50;
+            int Twenties = Convert.ToInt32(TwentiesText.Text) * 20;
+            int Tens = Convert.ToInt32(TensText.Text) * 10;
+            int Fives = Convert.ToInt32(FivesText.Text) * 5;
+            int Ones = Convert.ToInt32(OnesText.Text);
+
+            int Total = (Hundreds + Fiftys + Twenties + Tens + Fives + Ones);
+
+            TotalText.Text = Total.ToString();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            SetTextBoxes();
+        }
+    }
+}
